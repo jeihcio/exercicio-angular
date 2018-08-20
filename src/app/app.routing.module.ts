c1beb1cc3d39429f778278477e2427d3283f3c0f
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-08-20 16:31:46 -0300

    criando página 404

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,14 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from "./errors/not-found/not-found.component";
 
 const routes: Routes = [
     { path: 'user/flavio', component: PhotoListComponent },
-    { path: 'p/add', component: PhotoFormComponent}
+    { path: 'p/add', component: PhotoFormComponent},
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-    imports: [ RouterModule.forRoot(routes) ],
+    imports: [ 
+        RouterModule.forRoot(routes) 
+    ],
     exports: [ RouterModule ]
 })
 export class AppRoutingModule {}

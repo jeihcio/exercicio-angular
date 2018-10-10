4139a3110cc6789d12dd2cf76c5579b9e8bb0881
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-10-10 17:05:35 -0300

    output property

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from "@angular/core";
-import { PhotoFormComponent } from "./photo-form/photo-form.component";
 import { PhotoListModule } from "./photo-list/photo-list-module";
 import { PhotoModule } from "./photo/photo.module";
 import { PhotoFormModule } from "./photo-form/photo-form-module";

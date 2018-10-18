4a36d70a8ed43a5f6db4df02b15c911a874c4c8c
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-10-18 15:27:35 -0300

    signup

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
 import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
+import { SignUpComponent } from "./signup/signup.component";
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent 
+    ],
     imports: [ 
         CommonModule, 
         ReactiveFormsModule,
+        FormsModule,
         VMessageModule,
         RouterModule
     ]

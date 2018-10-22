005f529bb1ee941846d798d0ae689238a4a12496
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-10-22 11:08:04 -0300

    rotas filhas

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,18 +5,20 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
 import { SignUpComponent } from "./signup/signup.component";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
     declarations: [ 
         SignInComponent,
-        SignUpComponent 
+        SignUpComponent,
+        HomeComponent
     ],
     imports: [ 
         CommonModule, 
         ReactiveFormsModule,
         FormsModule,
         VMessageModule,
-        RouterModule
+        RouterModule        
     ]
 })
 export class HomeModule {}

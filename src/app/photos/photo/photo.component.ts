9e73d01f9fd79f824efae1d2dae0ff6abe799055
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-10-23 11:17:16 -0300

    arrumando url do upload

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,11 +1,27 @@
 import { Component, Input } from "@angular/core";
 
+const cloud = 'http://localhost:3000/imgs/';
+
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 
 export class PhotoComponent {
+
+    private _url = '';
+
     @Input() description = '';
-    @Input() url = '';
+
+    @Input() set url(url: string) {
+        if(url && url.startsWith('data')) {
+            this._url = url;
+        } else {
+            this._url = cloud + url;
+        }
+    }
+
+    get url(){
+        return this._url;
+    }
 };

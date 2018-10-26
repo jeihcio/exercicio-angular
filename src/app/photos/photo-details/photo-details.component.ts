25c02860df5b64546d0bf6fc7ede7b32152d8dd1
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-10-26 10:37:41 -0300

    detalhes

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { PhotoService } from "../photo/photo.service";
+import { Photo } from "../photo/photo";
+import { Observable } from "rxjs";
 
 @Component({
-    templateUrl: './photo-details.component.html'
+    templateUrl: './photo-details.component.html',
+    styleUrls: ['photo-details.css']
 })
 export class PhotoDetailsComponent implements OnInit{
 
-    constructor(private route: ActivatedRoute) {}
+    photo$: Observable<Photo>;
 
-    ngOnInit(): void {
-        const id = this.route.snapshot.params.photoId;
+    constructor(
+        private route: ActivatedRoute,
+        private photoService: PhotoService
+    ) {}
+
+    ngOnInit(): void {        
+        this.photo$ = this.photoService.findById(
+            this.route.snapshot.params.photoId
+        );
     }
 }

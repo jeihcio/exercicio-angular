87718220f9cdb7af4eecffd26ed7206ce28b3d97
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-10-26 15:14:57 -0300

    comentários

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -3,6 +3,7 @@ import { Observable } from "rxjs";
 import { PhotoComment } from "../../photo/photo-comment";
 import { PhotoService } from "../../photo/photo.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { switchMap, tap } from "rxjs/operators";
 
 @Component({
     selector: 'ap-photo-comments',
@@ -24,5 +25,16 @@ export class PhotoCommentsComponent implements OnInit {
         this.commentForm = this.formBuilder.group({
             comment: ['', Validators.maxLength(300)]
         });
+    }
+
+    save() {
+        const comment = this.commentForm.get('comment').value as string;
+        
+        this.comments$ = this.photoService
+            .addComment(this.photoId, comment)
+            .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+            .pipe(tap(() => {
+                this.commentForm.reset();
+            }));
     }
 }

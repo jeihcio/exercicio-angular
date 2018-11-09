c86ba00583af43d2a2e77098ccada8f5d00dfaff
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-11-09 16:06:34 -0200

    página de error

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -1,13 +1,19 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GlobalErrorComponent } from './global-error/global-error.component';
 import { GlobalErrorHandler } from './global-error-hardler/global-error-hardler';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [NotFoundComponent],
+  declarations: [
+    NotFoundComponent,
+    GlobalErrorComponent
+  ],
   providers: [
     {
       provide: ErrorHandler,
